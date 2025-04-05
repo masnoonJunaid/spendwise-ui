a9ac05bb6e0bf1a7963bd365c25a869a185e2854
masnoonjunaid <masnoon20@gmail.com>
2025-04-05 17:02:17 +0530

    protected route config with middleware

--- middleware.ts
+++ middleware.ts
@@ -2,6 +2,20 @@ import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
 export function middleware(request: NextRequest) {
-  // Your logic here
-  return NextResponse.next()
+  const url = request.nextUrl.pathname;
+  const token = request.cookies.get("access_token"); // fix typo from "acess_token" to "access_token"
+
+  const isAccessingDashboard = url.startsWith("/dashboard");
+
+  if (isAccessingDashboard && !token) {
+    // Not authenticated, redirect to homepage or login
+    return NextResponse.redirect(new URL("/", request.url));
+  }
+
+  if (url === "/" && token) {
+    // Already logged in, redirect to dashboard
+    return NextResponse.redirect(new URL("/dashboard", request.url));
+  }
+
+  return NextResponse.next();
 }
