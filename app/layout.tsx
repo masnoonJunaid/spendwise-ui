1abd8bd055a5e9fc1b9b1f2e611e37279c3761b9
masnoonjunaid <masnoon20@gmail.com>
2025-04-07 18:28:01 +0530

    updated layout text

--- app/layout.tsx
+++ app/layout.tsx
@@ -48,14 +48,14 @@ export default function RootLayout({
           <Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
             <div className="relative flex flex-col min-h-screen">
               <Navbar/>
-              <main className="container mx-auto max-w-7xl pt-16 px-6 flex-grow">
+              <main className="container mx-auto max-w-7xl pt-10 px-6 flex-grow">
                 {children}
               </main>
               <footer className="w-full flex items-center justify-center py-3">
                 <Link
                   isExternal
                   className="flex items-center gap-1 text-current"
-                  href="https://heroui.com?utm_source=next-app-template"
+                  href="/"
                   title="heroui.com homepage"
                 >
                   <span className="text-default-600">Powered by</span>
