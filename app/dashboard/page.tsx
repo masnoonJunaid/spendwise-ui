28cbbaa7f0db5271009ed5efe0b7057db0d11088
masnoonjunaid <masnoon20@gmail.com>
2025-04-07 18:48:07 +0530

    dynamic dashboard rendering

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,16 +1,28 @@
 "use client";
-import ChartComponent from "@/components/ChartComponents";
-import ToolsComponent from "@/components/ToolsComponents";
+
+import dynamic from "next/dynamic";
+
+// Dynamically import components to disable server-side rendering
+const ChartComponent = dynamic(() => import("@/components/ChartComponents"), {
+  ssr: false,
+});
+
+const ToolsComponent = dynamic(() => import("@/components/ToolsComponents"), {
+  ssr: false,
+});
 
 export default function Dashboard() {
   return (
     <div className="relative h-screen">
-        <div className="w-[66vw] h-[calc(100vh-64px)] mt-0 p-4">
-            <ChartComponent />
-        </div>
-        <div className="fixed top-16 right-0 h-[calc(100vh-64px)] w-[24vw] p-4">
-            <ToolsComponent />
-        </div>
+      {/* Main chart area */}
+      <div className="w-[66vw] h-[calc(100vh-64px)] mt-0 p-4">
+        <ChartComponent />
+      </div>
+
+      {/* Sidebar tools */}
+      <div className="fixed top-16 right-0 h-[calc(100vh-64px)] w-[24vw] p-4">
+        <ToolsComponent />
+      </div>
     </div>
   );
 }
